f1dc502e32152f269983939240cadb3f9c6e2c65
IchiFallUp <156977976+IchiFallUp@users.noreply.github.com>
2025-01-31 21:19:59 -0500

    Create Q1.java

---
+++ SENIOR/2024/Q1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main1{
+  public static void main(String[] args){
+    
+    Scanner input = new Scanner(System.in);
+    int intNumOfPeople = input.nextInt();
+    int[] circle = new int[intNumOfPeople];
+    
+    for(int i = 0; i < intNumOfPeople; i++) {
+      int intTemp = input.nextInt();
+      circle[i] = intTemp;
+    }
+    
+    int half = intNumOfPeople / 2;
+    int intCount = 0;
+    
+    for(int i = 0; i < half; i++){
+      if(circle[i] == circle[i + half]){
+        intCount+=2;
+      }
+    }
+    
+    System.out.println(intCount);
+  }
+}
